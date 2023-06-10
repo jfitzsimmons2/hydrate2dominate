3d90c322a5e5f23fe4e850245aefcecdcc0ae42e
Joe Fitzsimmons <jfitzsimmons2@gmail.com>
2023-06-10 15:24:15 -0400

    Remove console.log

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-//Get environment variables
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_KEY = import.meta.env.VITE_ANON_KEY;
-
-console.log(SUPABASE_URL, SUPABASE_KEY);
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY!);
